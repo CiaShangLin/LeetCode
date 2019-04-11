a891a53777727e3fd4d774053e95bdaa33f3059d
fcu-d0514534 <D0514534@gmail.com>
2019-04-11 22:09:29 +0800

    findMedianSortedArrays

--- src/Solution.kt
+++ src/Solution.kt
@@ -1,10 +1,36 @@
 fun main(args: Array<String>) {
-
-
+    findMedianSortedArrays(intArrayOf(1, 2), intArrayOf(3, 5))
 
 
 }
 
+fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
+    var list = mutableListOf<Int>()
+    var i: Int = 0
+    var j: Int = 0
+    while (i < nums1.size && j < nums2.size) {
+        if (nums1[i] < nums2[j]) {
+            list.add(nums1[i++])
+        } else {
+            list.add(nums2[j++])
+        }
+    }
+    if (i < nums1.size) {
+        for (index in i..nums1.size - 1) {
+            list.add(nums1[index])
+        }
+    }
+    if (j < nums2.size) {
+        for (index in j..nums2.size - 1) {
+            list.add(nums2[index])
+        }
+    }
 
+    var size = list.size
 
+    if (size % 2 != 0) {
+        return list[size / 2].toDouble()
+    }
+    return ((list[size / 2] + list[size / 2 - 1]) / 2.0).toDouble()
 
+}
